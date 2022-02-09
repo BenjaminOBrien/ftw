eacc32d9c088599cd2f419c4b722f165af4fc9b7
Benjamin <91637620+BenjaminOBrien@users.noreply.github.com>
2022-02-09 16:31:00 -0600

    Update new_drone.js

--- new_drone.js
+++ new_drone.js
@@ -1,13 +1,16 @@
 // Honk, cool program
 startProgram = async function () {
   await drone.takeOff();
-  await drone.fly("up", 0.8, 100);
+  await drone.fly("up", 0.9, 100);
   await drone.fly("right", 0.3, 100);
   await drone.fly("forward", 1, 100);
-  await drone.fly("down", 0.9, 100);  // Go down more so we can hover over the thing
+  await drone.fly("down", 1.1, 100);  // Go down more so we can hover over the thing
   await drone.fly("left", 0.5,  100);
-  await drone.fly("backward", 0.4, 100);
+  
+  // yooo wait until the balls be gone
   await drone.wait(3);
+  
+  // aIGHT IMA HEAD OUT
   await drone.rotate(45, "clockwise");
   await drone.fly("forward", 2, 100);
   await drone.land();
